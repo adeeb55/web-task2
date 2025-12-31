a1b11186d01e6be6aebca9a89d50f0a14c513cae
adeeb55 <adeebkhanfar02@gmail.com>
2025-12-31 18:20:40 +0200

    mom manipulation

--- java.js
+++ java.js
@@ -3,3 +3,21 @@ let filterStatus = "all";
 let currentlyEditingTaskId = null;
 let currentlyDeletingTaskId = null;
 const regex = /^[0-9]/;
+const toDoForm = document.getElementById("toDoInputForm");
+const toDoInput = document.getElementById("toDoInput");
+const tasksList = document.getElementById("tasksList");
+const filterBtnAll = document.getElementById("filterBtnAll");
+const filterBtnDone = document.getElementById("filterBtnDone");
+const filterBtnTodo = document.getElementById("filterBtnTodo");
+const popupCancelBtn = document.getElementById("popupCancelBtn");
+const beEmpty = document.getElementById("beEmpty");
+const startWithNumber = document.getElementById("startWithNumber");
+const fiveCharLong = document.getElementById("fiveCharLong");
+const popupBeEmpty = document.getElementById("popupBeEmpty");
+const popupStartWithNumber = document.getElementById("popupStartWithNumber");
+const popupFiveCharLong = document.getElementById("popupFiveCharLong");
+const deleteAllBtn = document.getElementById("deleteAllBtn");
+const deleteDoneBtn = document.getElementById("deleteDoneBtn");
+const popupDeleteAllCancelBtn = document.getElementById(
+  "popupDeleteAllCancelBtn"
+);
